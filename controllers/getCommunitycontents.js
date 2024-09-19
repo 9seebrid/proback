1da3d2c621fbde9a8d21921015a52d19db2545d1
bonse koo <bonse88@hs.ac.kr>
2024-09-19 17:39:51 +0900

    getcommunity

--- controllers/getCommunitycontents.js
+++ controllers/getCommunitycontents.js
@@ -1,10 +1,8 @@
 const database = require('../database/database');
 
 exports.getAnnouncement = async (req, res) => {
-  const announcement = req.params.announcement;
-
   try {
-    const result = await database.query('SELECT * FROM community where write_number=$1', [announcement]);
+    const result = await database.query('SELECT * FROM community ORDER BY write_number');
     return res.status(200).json(result.rows);
   } catch (error) {
     return res.status(500).json({ msg: 'Get Items Fail' + error });
