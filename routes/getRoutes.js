9cb9d61d9078401166402bc8c3a7b130f74a0df4
bonse koo <bonse88@hs.ac.kr>
2024-09-19 16:45:22 +0900

    get

--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -6,15 +6,11 @@ const { getCart } = require('../controllers/getCart');
 const { eachPurchase } = require('../controllers/eachPurchase');
 const { getAnnouncement } = require('../controllers/getCommunitycontents');
 
-
-
-
 router.get('/get_products/', getProducts);
 router.get('/get_detail_products/:productId', getDetailproduct);
 router.get('/get_cart/:userId', getCart);
-router.get('/get_product/', getProduct)
-router.get('/each_purchase/:userId', eachPurchase );
-router.get('/community_announcement/', getAnnouncement );
-
+router.get('/get_product/', getProduct);
+router.get('/each_purchase/:userId', eachPurchase);
+router.get('/community_announcement/', getAnnouncement);
 
 module.exports = router; // router 변수를 모듈로 사용할 수 있도록 설정
