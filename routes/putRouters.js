39a484322a62a0bc25b657d14a38835070f2f1d5
bonse koo <bonse88@hs.ac.kr>
2024-09-20 12:58:14 +0900

    delete

--- routes/putRouters.js
+++
@@ -1,5 +0,0 @@
-const { updateViewCount } = require('../controllers/putViews');
-
-const router = require('express').Router();
-
-router.put('/community_detail/view/:write_number', updateViewCount);
