4736585632dd6bda817920535fe1886fd2c38ad0
bonse koo <bonse88@hs.ac.kr>
2024-09-20 09:40:04 +0900

    get update

--- controllers/getCommunitycontents.js
+++ controllers/getCommunitycontents.js
@@ -8,3 +8,12 @@ exports.getAnnouncement = async (req, res) => {
     return res.status(500).json({ msg: 'Get Items Fail' + error });
   }
 };
+
+exports.getComminityDetail = async (req, res) => {
+  try {
+    const result = await database.query('SELECT * FROM community where write_number=$1', [req.params.write_number]);
+    return res.status(200).json(result.rows);
+  } catch (error) {
+    return res.status(500).json({ msg: 'Get Items Fail' + error });
+  }
+};
